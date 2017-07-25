7fc1fb6d1d43fa673a10b937b9e7e232c75364e6
Jared Romney <jared.romney28@gmail.com>
2017-07-25 14:28:43 -0600

    added 'r'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
 Rails.application.configure do
-  
+
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = :uglifie
+  config.assets.js_compressor = :uglifier
   config.assets.compile = true
   config.log_level = :debug
   config.log_tags = [ :request_id ]
